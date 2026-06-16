07061980ba0d6ce8aac18ed8bd37d013b45ad14f
ttetrafon <ttetrafon@yahoo.gr>
2026-06-16 14:32:40 +0300

    updated dependencies

--- workers/app.ts
+++ workers/app.ts
@@ -32,6 +32,9 @@ export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
 
+		if (url.pathname.startsWith('/api')) {
+			return handleApiRequest(url, request, env);
+		}
 
 		return requestHandler(request, {
 			cloudflare: { env, ctx },
